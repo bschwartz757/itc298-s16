a44c9e941d0b77f4f92b4eb47c7167ce1c615d6d
Blake Schwartz <blakehschwartz@gmail.com>
2016-04-05 21:22:32 -0700

    April 5th

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.writeline("This is the index.js file");
+var http = require("http"), fs = require('fs');
+
+http.createServer(function(req, res){
+  console.log("test 1");
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("Aloha world");
+
+}).listen(3000);
+console.log("test 2");
