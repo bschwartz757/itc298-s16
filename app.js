02e64640bd0485a564a45b13f72fe4be4be92838
Blake Schwartz <blakehschwartz@gmail.com>
2016-05-14 19:44:31 -0700

    Changes to all files for Assignment 4

--- app.js
+++ app.js
@@ -28,66 +28,45 @@ app.get('/headers', function(req, res){
   res.send(s);
 });
 
-//home/about routes
+// home/about routes
 app.get('/', function(req, res){
   res.render('home', {books: book.getAll()});
-});
-app.get('/about', function(req, res){
-  res.render('about');
-});
-
-app.post('/detail', function(req, res){
-  res.type('text/html');
-  var result = book.get(req.body.title);
-  if(result) {
-    res.render('detail', {book: result.book, length: result.length});
-   } else {
-    var newBook = {"title": req.body.title};
-    res.render('detail', {book: newBook});
-  }
 });
 
 app.get('/detail/:title', function(req, res){
   res.type('text/html');
-  var result = book.get(req.params.title);
-  if(result) {
-    res.render('detail', {book: result.book, length: result.length});
-   } else {
-    var newBook = {"title": req.params.title};
-    res.render('detail', {book: newBook});
+  var found = book.get(req.params.title);
+  if(!found) {
+    found = {title: req.params.title};
   }
+  res.render('detail', {book: found});
+});
+
+app.get('/about', function(req, res){
+  res.type('text/html');
+  res.render('about');
 });
 
 app.post('/search', function(req, res){
   res.type('text/html');
-  var result = book.get(req.body.title);
-  if(result) {
-    res.render('detail', {book: result.book, length: result.length});
-   } else {
-    var newBook = {"title": req.body.title};
-    res.render('detail', {book: newBook});
-  }
+  var found = book.get(req.body.title);
+  if(!found) {
+    found = {title: req.body.title};
+   }
+    res.render('detail', {book: found});
 });
 
 app.post('/add', function(req, res){
   res.type('text/html');
   var newBook = {"id": req.body.id, "title": req.body.title, "author": req.body.author};
   var result = book.add(newBook);
-  if(found){
-    res.render('detail', {book: result.book});
-  } else {
-    res.render('detail', {book: newBook, result: result});
-  }
+  res.render('detail', {book: newBook, result: result});
 });
 
 app.post('/delete', function(req, res){
   res.type('text/html');
   var result = book.delete(req.body.title);
-  if(deleted){
-    res.render('detail', {result: book.delete(req.body.title)});
-  } else {
-    res.render('detail', {result: books.length})
-  }
+  res.render('detail', {result: book.delete(req.body.title)});
 });
 
 //404 catch-all handler (middleware)
