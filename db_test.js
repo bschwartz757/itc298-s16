430a27cc0c85491bd92e0d4a640e757858d7fc22
Blake Schwartz <blakehschwartz@gmail.com>
2016-06-30 21:54:26 -0700

    Adds less compiled bootstrap, enhancements to app

--- db_test.js
+++ db_test.js
@@ -3,7 +3,12 @@
  */
 var Book = require('./models/book.js');
 
-new Book({title: 'Hamlet', author: 'William Shakespeare'}).save();
+new Book({title: 'hamlet', author: 'william shakespeare', price: 24.50}).save();
+new Book({title: 'moby dick', author: 'herman melville', price: 22.00}).save();
+new Book({title: 'the scarlet letter', author: 'nathaniel hawthorne', price: 21.25}).save();
+new Book({title: 'great expectations', author: 'charles dickens', price: 37.85}).save();
+new Book({title: 'don quixote', author: 'miguel de cervantez', price: 35.00}).save();
+new Book({title: 'the merchant of venice', author: 'william shakespeare', price: 45.00}).save();
 
 Book.find(function(err, books){
   console.log(books);
--- models/book.js
+++ models/book.js
@@ -1,16 +1,16 @@
 /**
  * Created by blakers757 on 5/17/2016.
  */
-// var credentials = require('../lib/credentials');
+var credentials = require('../lib/credentials');
 var mongoose = require('mongoose');
 
 //remote db
-// var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }  } };
-// mongoose.connect(credentials.mongo.connectionString, options)
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }  } };
+mongoose.connect(credentials.mongo.connectionString, options)
 
 //local db
-var ip = '127.0.0.1';
-mongoose.connect('mongodb://' + ip + '/projects');
+// var ip = '127.0.0.1';
+// mongoose.connect('mongodb://' + ip + '/projects');
 
 var conn = mongoose.connection;
 conn.on('error', console.error.bind(console, 'connection error:'));
